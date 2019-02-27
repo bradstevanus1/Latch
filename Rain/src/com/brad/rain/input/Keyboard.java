d33276ccbe74a32b6c51d1a8d6c9e28d44878ddf
Brad <bradstevanus1@gmail.com>
2019-02-27 14:09:40 -0500

    Added game logic to switch camera modes between locked or unlocked, currently assigned to g key

--- Rain/src/com/brad/rain/input/Keyboard.java
+++ Rain/src/com/brad/rain/input/Keyboard.java
@@ -1,12 +1,14 @@
 package com.brad.rain.input;
 
+import com.brad.rain.Game;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Keyboard implements KeyListener {
 
     private boolean[] keys = new boolean[120]; // or 65536
-    public boolean up, down, left, right, escape;
+    public boolean up, down, left, right, escape, screenLockToggle;
 
     public void update() {
         up = keys[KeyEvent.VK_UP] || keys[KeyEvent.VK_W];
@@ -14,6 +16,7 @@ public class Keyboard implements KeyListener {
         left = keys[KeyEvent.VK_LEFT] || keys[KeyEvent.VK_A];
         right = keys[KeyEvent.VK_RIGHT] || keys[KeyEvent.VK_D];
         escape = keys[KeyEvent.VK_ESCAPE];
+        screenLockToggle = keys[KeyEvent.VK_G];
     }
 
     @Override
@@ -28,7 +31,6 @@ public class Keyboard implements KeyListener {
 
     @Override
     public void keyTyped(KeyEvent e) {
-
     }
 
 }
